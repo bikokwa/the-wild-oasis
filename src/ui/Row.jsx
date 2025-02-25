1b904539b55a1aac6337a3af4f77b9f1dd4f96c4
bikokwa <34458781+bikokwa@users.noreply.github.com>
2025-02-25 14:04:32 +0300

    app layout

--- src/ui/Row.jsx
+++ src/ui/Row.jsx
@@ -1,11 +1,10 @@
 import styled, { css } from "styled-components";
 
 const Row = styled.div`
-  display: flex;
-
   ${(props) =>
     props.type === "horizontal" &&
     css`
+      display: flex;
       justify-content: space-between;
       align-items: center;
     `}
@@ -13,6 +12,7 @@ const Row = styled.div`
   ${(props) =>
     props.type === "vertical" &&
     css`
+      display: flex;
       flex-direction: column;
       gap: 1.6rem;
     `}
